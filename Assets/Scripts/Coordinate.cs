43bf12541525cd5c08c99e75307e0097eb0e96a7
dj46563 <tylrpage@gmail.com>
2020-07-27 21:10:29 -0700

    Created coordinate struct

---
+++ Assets/Scripts/Coordinate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct Coordinate
+{
+    public char column;
+    public byte row;
+}
