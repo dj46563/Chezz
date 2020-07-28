8335d70209ac46ee17fa91ba515db429aac5487f
Junmin Yee <junminyee98@gmail.com>
2020-07-28 12:47:03 -0700

    Create AbstractPiece

---
+++ Assets/Scripts/AbstractPiece.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractPiece : MonoBehaviour
+{
+    public bool isWhite { get; set; }
+    public int pointValue { get; set; }
+    public Coordinate position { get; set; }
+
+    public abstract Coordinate[] GetMoves();
+}
