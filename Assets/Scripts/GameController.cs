62a6998c1e91724d7f269b926ca62e73b8946066
dj46563 <tylrpage@gmail.com>
2020-07-27 23:05:11 -0700

    Added listeners to game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,8 +11,27 @@ public class GameController : MonoBehaviour
     private void Awake()
     {
         if (Application.isBatchMode)
+        {
             _chessNetworker = new ChessNetworker(ChessNetworker.NetworkerType.Server, gameObject);
+            _chessNetworker.ClientMoveReceived += OnClientMoveReceived;
+        }
         else
+        {
             _chessNetworker = new ChessNetworker(ChessNetworker.NetworkerType.Client, gameObject);
+            _chessNetworker.ServerMoveReceived += OnServerMoveReceived;
+        }
+    }
+
+    private void OnServerMoveReceived(Coordinate arg1, Coordinate arg2, TimeSpan arg3)
+    {
+        // Apply move to board
+        throw new NotImplementedException();
+    }
+
+    private void OnClientMoveReceived(Coordinate arg1, Coordinate arg2, uint arg3)
+    {
+        // Check if move is valid
+        // send move to all clients
+        throw new NotImplementedException();
     }
 }
