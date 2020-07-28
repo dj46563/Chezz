8d4dfec521c38a977761cc459e86bbfe403f296f
dj46563 <tylrpage@gmail.com>
2020-07-27 20:46:09 -0700

    Server and client created

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ENet;
+using Event = ENet.Event;
+using EventType = ENet.EventType;
+
+public class Client : MonoBehaviour
+{
+    public event Action<byte[]> PacketReceived;
+    
+    private Address _address;
+    private Host _client;
+    private Peer _peer;
+    
+    public void Connect(string host, ushort port)
+    {
+        _address = new Address();
+        _client = new Host();
+        
+        _address.SetHost(host);
+        _address.Port = port;
+        _client.Create();
+
+        _peer = _client.Connect(_address);
+    }
+
+    public void SendData(byte[] data, bool reliable = true)
+    {
+        Packet packet = default(Packet);
+        PacketFlags packetFlags = PacketFlags.None;
+        if (reliable)
+            packetFlags |= PacketFlags.Reliable;
+        packet.Create(data, packetFlags);
+
+        _peer.Send(0, ref packet);
+    }
+
+    private void LateUpdate()
+    {
+        bool polled = false;
+        Event netEvent;
+
+        while (!polled)
+        {
+            if (_client.CheckEvents(out netEvent) <= 0)
+            {
+                if (_client.Service(0, out netEvent) <= 0)
+                    return;
+                
+                polled = true;
+            }
+
+            switch (netEvent.Type)
+            {
+                case EventType.None:
+                    break;
+                case EventType.Connect:
+                    Debug.Log("Client connected to server");
+                    break;
+                case EventType.Disconnect:
+                    Debug.Log("Client disconnected from server");
+                    break;
+                case EventType.Timeout:
+                    Debug.Log("Client timed out");
+                    break;
+                case EventType.Receive:
+                    byte[] buffer = new byte[netEvent.Packet.Length];
+                    PacketReceived?.Invoke(buffer);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ENet;
+using Event = ENet.Event;
+using EventType = ENet.EventType;
+
+public class Server : MonoBehaviour
+{
+    public event Action<byte[], uint> PacketReceived; 
+    
+    private Host _server;
+    private Address _address;
+
+    public void Listen(ushort port, string hostName = null)
+    {
+        _address = new Address();
+        _server = new Host();
+        
+        if (hostName != null)
+            _address.SetHost(hostName);
+        _address.Port = port;
+        
+        _server.Create(_address, Constants.MaxClients);
+    }
+
+    public void BroadcastData(byte[] data, bool reliable = true)
+    {
+        Packet packet = default(Packet);
+        PacketFlags packetFlags = PacketFlags.None;
+        if (reliable)
+            packetFlags |= PacketFlags.Reliable;
+        packet.Create(data, packetFlags);
+        
+        _server.Broadcast(0, ref packet);
+    }
+
+    private void LateUpdate()
+    {
+        bool polled = false;
+        Event netEvent;
+
+        while (!polled)
+        {
+            if (_server.CheckEvents(out netEvent) <= 0)
+            {
+                if (_server.Service(0, out netEvent) <= 0)
+                    return;
+
+                polled = true;
+            }
+
+            switch (netEvent.Type)
+            {
+                case EventType.None:
+                    break;
+                case EventType.Connect:
+                    Debug.Log("Client connected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                    break;
+                case EventType.Disconnect:
+                    Debug.Log("Client disconnected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                    break;
+                case EventType.Timeout:
+                    Debug.Log("Client timed out - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                    break;
+                case EventType.Receive:
+                    byte[] buffer = new byte[netEvent.Packet.Length];
+                    PacketReceived?.Invoke(buffer, netEvent.Peer.ID);
+                    break;
+            }
+        }
+    }
+}
