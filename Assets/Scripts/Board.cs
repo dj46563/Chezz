7d2d64414f5d43b990ab7ed2cd59b18a74399fe8
dj46563 <tylrpage@gmail.com>
2020-08-03 11:00:14 -0700

    Added public Networking bool to board, if checked, board will only move a piece if the server confirms it

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,10 +7,13 @@ public enum ChessPiece { Pawn, Knight, Bishop, Rook, Queen, King };
 
 public class Board : MonoBehaviour
 {
+    public bool Networked;
+    
     Piece[] board;
 
     bool isPieceClicked = false;
     Piece pieceClicked;
+    private bool gameStarted = false;
     public event Action<Coordinate, Coordinate> OnPieceMove;
 
     // Start is called before the first frame update
@@ -19,10 +22,15 @@ public class Board : MonoBehaviour
         board = gameObject.GetComponent<BoardSpawner>().SpawnBoard();
     }
 
+    private void Awake()
+    {
+        UIController.MainMenuUnloaded += () => { gameStarted = true; };
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && gameStarted)
         {
             Coordinate square = CoordinateHelper.Vector3ToCoordinate(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             if (isPieceClicked)
@@ -30,10 +38,11 @@ public class Board : MonoBehaviour
                 // Update when clicking new tile on board
                 if (square != pieceClicked.Position)
                 {
-                    OnPieceMove?.Invoke(pieceClicked.Position, square); 
-                    board[CoordinateHelper.CoordinateToArrayIndex(pieceClicked.Position)] = null;
-                    pieceClicked.MoveTo(square);
-                    board[CoordinateHelper.CoordinateToArrayIndex(square)] = pieceClicked;
+                    OnPieceMove?.Invoke(pieceClicked.Position, square);
+                    if (!Networked)
+                    {
+                        MoveTo(pieceClicked.Position, square);
+                    }
                 }
                 isPieceClicked = false;
             }
@@ -45,7 +54,8 @@ public class Board : MonoBehaviour
         }
     }
 
-    public void MoveTo(Coordinate src, Coordinate dest)
+    // Returns true if move was valid
+    public bool MoveTo(Coordinate src, Coordinate dest)
     {
         Piece piece = board[CoordinateHelper.CoordinateToArrayIndex(src)];
         if (piece != null)
@@ -53,6 +63,10 @@ public class Board : MonoBehaviour
             board[CoordinateHelper.CoordinateToArrayIndex(src)] = null;
             piece.MoveTo(dest);
             board[CoordinateHelper.CoordinateToArrayIndex(dest)] = piece;
+
+            return true;
         }
+
+        return false;
     }
 }
